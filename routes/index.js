2721a15f7b9fad4991b40683c6abac3312c3a5be
Paul Shivers <paulsshivers@gmail.com>
2019-10-16 17:38:39 -0400

    hopefully final name change for billRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(require('./billsRoutes'));
+router.use(require('./billRoutes'));
 router.use(require('./copayerRoutes'));
 router.use(require('./monthRoutes'));
 
