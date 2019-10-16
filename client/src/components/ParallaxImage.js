c579658bedf33e58876752b5389bdedbd1f3e84c
Paul Shivers <paulsshivers@gmail.com>
2019-10-16 17:28:35 -0400

    changed case again of billroutes

--- client/src/components/ParallaxImage.js
+++ client/src/components/ParallaxImage.js
@@ -3,7 +3,7 @@ import React from 'react';
  
 const ParallaxImage = () => (
     <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
-        <img src='https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80' />
+        <iherokmg src='https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80' />
     </Parallax>
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(require('./billRoutes'));
+router.use(require('./billroutes'));
 router.use(require('./copayerRoutes'));
 router.use(require('./monthRoutes'));
 
