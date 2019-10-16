5a5387c28556cf4ee33221ee21f3c35ba5bda030
Paul Shivers <paulsshivers@gmail.com>
2019-10-16 18:31:57 -0400

    removed / from static dir

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(routes);
 
 app.use(express.static(`${__dirname}/client/build`));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`);
 });
 
