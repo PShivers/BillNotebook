42303ad699a9a721b7e66d5da04c1ad2f3c10cd7
Paul Shivers <paulsshivers@gmail.com>
2019-10-16 18:00:14 -0400

    removed build from gitignore

---
+++ client/build/precache-manifest.896687425027f121270a238ccffa3c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d935a9f5429dacaafdc01c94179527e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e87d5a7cb0ffbf634d27",
+    "url": "/static/css/2.7ae64611.chunk.css"
+  },
+  {
+    "revision": "1d026a2da46e65d1c07e",
+    "url": "/static/css/main.ab86d79a.chunk.css"
+  },
+  {
+    "revision": "e87d5a7cb0ffbf634d27",
+    "url": "/static/js/2.e3de5768.chunk.js"
+  },
+  {
+    "revision": "1d026a2da46e65d1c07e",
+    "url": "/static/js/main.3b775cfb.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(require('./billRoutes'));
+router.use(require('./billRoqutes'));
 router.use(require('./copayerRoutes'));
 router.use(require('./monthRoutes'));
 
